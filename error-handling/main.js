45a16e44f67991328342e979f8a7901a824b043d
Tomi Scattini <tomiscattini@Tomis-Mac-mini.local>
2026-02-14 20:28:43 +0100

    Feature: prepare error-handling examples

--- error-handling/main.js
+++ error-handling/main.js
@@ -1,1 +1,22 @@
+function renderMessageOnScreen(message) {
+  const rootElement = document.querySelector("#app");
+
+  if (!!rootElement) {
+    const normalHeadingElement = document.createElement("h2");
+    normalHeadingElement.innerText = message ?? "";
+    rootElement.appendChild(normalHeadingElement);
+  }
+}
+
+function renderErrorOnScreen(errorMessage) {
+  const rootElement = document.querySelector("#app");
+
+  if (!!rootElement) {
+    const errorHeadingElement = document.createElement("h2");
+    errorHeadingElement.innerText = errorMessage ?? "";
+    errorHeadingElement.classList.add("error-message");
+    rootElement.appendChild(errorHeadingElement);
+  }
+}
+
 document.addEventListener("DOMContentLoaded", () => {});
